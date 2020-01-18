f0b76320f16b9ee36221ea1ea385267173f51a84
nsi6000 <simon.nicolas@outlook.be>
2020-01-18 12:08:39 +0100

    added unittests to fizzbuzz.

--- programming_challenges/python/fizzbuzz.py
+++ programming_challenges/python/fizzbuzz.py
@@ -1,11 +1,25 @@
 
+import unittest
 
-for i in range(1,101):
-    if i%3 == 0 and i%5 == 0:
-        print("FizzBuzz")
-    elif i%3 == 0:
-        print("Fizz")
-    elif i%5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+class test_fizzbuzz(unittest.TestCase):
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(1),1)
+        self.assertEqual(fizzbuzz(60),"FizzBuzz")
+        self.assertEqual(fizzbuzz(69),"Fizz")
+        self.assertEqual(fizzbuzz(100),"Buzz")
+
+def fizzbuzz(i):
+#    for i in range(1,101):
+        if i%3 == 0 and i%5 == 0:
+            return "FizzBuzz"
+        elif i%3 == 0:
+            return "Fizz"
+        elif i%5 == 0:
+            return "Buzz"
+        else:
+            return i
+
+if __name__ == '__main__':
+    unittest.main()
+    fizzbuzz()
+
