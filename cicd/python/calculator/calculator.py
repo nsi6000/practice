18927bb0a175d131ba05bc7225295c9dfc5adf07
Nicolas Simon <simon.nicolas@outlook.be>
2020-01-26 10:38:48 +0000

    Init of RealPython calculator.

---
+++ cicd/python/calculator/calculator.py
@@ -0,0 +1,12 @@
+"""
+Calculator library containing basic math operations.
+source: realpython.com/python-continuous-integration/
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def substract(first_term, second_term):
+    return first_term - second_term
+
+
