8e006e77dc8b76421dc45c7174ca02d6ca076500
nsi6000 <60031770+nsi6000@users.noreply.github.com>
2024-05-24 19:07:21 +0200

    Create main.java

---
+++ codeforces/template/main.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class main {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int t = scanner.nextInt();
+    scanner.nextLine();
+    while(t-- > 0){
+      int n = scanner.nextInt();
+      //code here.
+    }
+    System.out.println("----------------");
+    scanner.close();
+  }
+}
