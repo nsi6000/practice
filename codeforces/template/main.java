f58c46df0e0e2879f362d7f470ed8888319384fb
nsi6000 <60031770+nsi6000@users.noreply.github.com>
2024-05-24 19:30:33 +0200

    Update main.java

--- codeforces/template/main.java
+++ codeforces/template/main.java
@@ -9,8 +9,8 @@ public class main {
     while(t-- > 0){
       int n = scanner.nextInt();
       //code here.
+      System.out.println("----------------");
     }
-    System.out.println("----------------");
     scanner.close();
   }
 }
